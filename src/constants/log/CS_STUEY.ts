014b9cbf22fe46bd97b4a3d42b6140c6db083022
박건규 <geongyu09@gmail.com>
2026-01-17 17:10:12 +0900

    feat: 로그 데이터 추가

---
+++ src/constants/log/CS_STUEY.ts
@@ -0,0 +1,80 @@
+import { LogListItem } from '@/types/log';
+
+const CS_STUDY: LogListItem[] = [
+  {
+    title: 'Part 01 네트워크 세상에 들어서며',
+    date: '2024-11-29',
+    href: 'https://geongyu09.notion.site/Part-01-2e7edd40d4948035b1f2033f3f4f4f6e?source=copy_link',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+  {
+    title: 'Part 02. 네트워크와 케이블, 그리고 친구들',
+    date: '2024-11-29',
+    href: 'https://geongyu09.notion.site/Part-02-14aedd40d4948004accfe53cebfdc87d',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+  {
+    title: 'Part03. TCP/IP와의 만남',
+    date: '2024-12-28',
+    href: 'https://geongyu09.notion.site/Part03-TCP-IP-166edd40d49480f599a7cc09470e02d9?source=copy_link',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+  {
+    title: 'Part04 네트워크 장비에 관한 이야기(1~5장)',
+    date: '2025-01-08',
+    href: 'https://geongyu09.notion.site/Part04-1-5-166edd40d4948057b0f8f0d050c81fff?source=copy_link',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+  {
+    title: 'Part 5. IP 주소로의 여행',
+    date: '2025-01-08',
+    href: 'https://geongyu09.notion.site/Part-5-IP-171edd40d4948074b8dfda8dace24ce2?source=copy_link',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+  {
+    title: 'Part6 스위치를 켜라',
+    date: '2025-01-14',
+    href: 'https://geongyu09.notion.site/Part6-177edd40d494802c8661ee5b9538c50b?source=copy_link',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+  {
+    title: 'VLAN',
+    date: '2025-02-04',
+    href: 'https://geongyu09.notion.site/VLAN-bb909b41210e4e26bc83b9674f215e6f?source=copy_link',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+  {
+    title: '라우터만 알면 네트워크 도사',
+    date: '2025-02-04',
+    href: 'https://geongyu09.notion.site/195edd40d494802c8a14f774c3bae83f?source=copy_link',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+  {
+    title: '라우팅 프로토콜과의 한판',
+    date: '2025-02-11',
+    href: 'https://geongyu09.notion.site/19cedd40d49480c79524ec8901d043e2?source=copy_link',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+  {
+    title: '라우터 그 속으로 전진',
+    date: '2025-02-18',
+    href: 'https://geongyu09.notion.site/1aaedd40d49480e7a972d03155e54128?source=copy_link',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+  {
+    title: '세상은 넓고 네트워킹은 계속된다.',
+    date: '2024-12-31',
+    href: 'https://geongyu09.notion.site/10-1b0edd40d494807aab8dfcc36da99829?source=copy_link',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+  {
+    title: '선이 없는 세상, 무선으로의 여행',
+    date: '2025-05-10',
+    href: 'https://geongyu09.notion.site/11-1b0edd40d49480ce9af3e722846e48b6?source=copy_link',
+    tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
+  },
+
+  //
+];
+
+export default CS_STUDY;
---
+++ src/constants/log/JAASSTUDY.ts
@@ -0,0 +1,60 @@
+import { LogListItem } from '@/types/log';
+
+const JAASSTUDY: LogListItem[] = [
+  {
+    title: 'Eval is Evil 😈',
+    date: '2024-06-13',
+    href: 'https://geongyu09.notion.site/Eval-is-Evil-2e7edd40d4948053a0b8fc3bd0394cde?source=copy_link',
+    tags: ['자아스스터디', '코어자바스크립트'],
+  },
+  {
+    title: '함수와 기명 함수',
+    date: '2024-06-13',
+    href: 'https://geongyu09.notion.site/2e7edd40d4948079ba6bca0c39d10606?source=copy_link',
+    tags: ['자아스스터디', '코어자바스크립트'],
+  },
+  {
+    title: 'var를 아직도 사용하시나요..? + 책 예제 let/var 비교',
+    date: '2024-06-13',
+    href: 'https://geongyu09.notion.site/var-let-var-2e7edd40d49480479344faa950c1fe16?source=copy_link',
+    tags: ['자아스스터디', '코어자바스크립트'],
+  },
+  {
+    title: 'this',
+    date: '2024-06-20',
+    href: 'https://geongyu09.notion.site/This-2e7edd40d49480258cf4f58c72572974?source=copy_link',
+    tags: ['자아스스터디', '코어자바스크립트'],
+  },
+  {
+    title: '전역 객체 관련',
+    date: '2024-06-20',
+    href: 'https://geongyu09.notion.site/2e7edd40d49480e6bc07e04902ef025f?source=copy_link',
+    tags: ['자아스스터디', '코어자바스크립트'],
+  },
+  {
+    title: '데코레이터',
+    date: '2024-06-20',
+    href: 'https://geongyu09.notion.site/2e7edd40d494801aaee1e1565d9ea301?source=copy_link',
+    tags: ['자아스스터디', '코어자바스크립트'],
+  },
+  {
+    title: 'Promise',
+    date: '2024-06-27',
+    href: 'https://geongyu09.notion.site/Promise-2e7edd40d49480709330f07c687a09cb?source=copy_link',
+    tags: ['자아스스터디', '코어자바스크립트'],
+  },
+  {
+    title: '리액트와 클로저',
+    date: '2024-07-04',
+    href: 'https://geongyu09.notion.site/2e7edd40d49480268750f8b7070dd9c9?source=copy_link',
+    tags: ['자아스스터디', '코어자바스크립트'],
+  },
+  {
+    title: '클래스',
+    date: '2024-07-11',
+    href: 'https://geongyu09.notion.site/2e7edd40d4948046a8f3c18e06d02473?source=copy_link',
+    tags: ['자아스스터디', '코어자바스크립트'],
+  },
+];
+
+export default JAASSTUDY;
