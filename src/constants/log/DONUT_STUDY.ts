d4a1d2434d125ab724dab7b3e75900bf96697c59
박건규 <geongyu09@gmail.com>
2026-01-17 17:10:12 +0900

    feat: 로그 데이터 추가

---
+++ src/constants/log/DONUT_STUDY.ts
@@ -0,0 +1,21 @@
+import { LogListItem } from '@/types/log';
+
+const DONUT_STUDY: LogListItem[] = [
+  {
+    title: 'React의 동등비교',
+    date: '2024-03-13',
+    href: 'https://geongyu09.notion.site/React-2e7edd40d494806e8d81fc476e9bc907?source=copy_link',
+  },
+  {
+    title: '컴포넌트와 훅은 순수해야 합니다!',
+    date: '2024-03-20',
+    href: 'https://geongyu09.notion.site/2e7edd40d4948065af28d94406dcd8b9?source=copy_link',
+  },
+  {
+    title: '이벤트 루프와 비동기 통신의 이해',
+    date: '2024-03-27',
+    href: 'https://geongyu09.notion.site/3-Knotted-7875c55c764c490db0b016d6755f2de2?source=copy_link',
+  },
+];
+
+export default DONUT_STUDY;
---
+++ src/constants/log/presentationData.ts
@@ -0,0 +1,3 @@
+const PRESENTATIONS = [];
+
+export default PRESENTATIONS;
