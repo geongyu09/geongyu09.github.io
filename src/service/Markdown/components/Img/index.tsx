9f5425aed6f70550e119d68650f3da4d696faae2
박건규 <geongyu09@gmail.com>
2026-04-25 00:49:39 +0900

    fix: 마크다운 이미지 최대 높이 및 중앙 정렬 스타일 추가

--- src/service/Markdown/components/Img/index.tsx
+++ src/service/Markdown/components/Img/index.tsx
@@ -16,7 +16,10 @@ export default function Img({
       height={height ? Number(height) : 1000}
       src={src ?? ''}
       alt={alt ?? 'post content'}
-      className={cn('my-14 rounded-xl block shadow-sm select-none')}
+      className={cn(
+        'my-14 rounded-xl block shadow-sm select-none',
+        'max-h-[600px] w-auto object-contain mx-auto',
+      )}
     />
   );
 }
