a78b724c376af5f6d5c38de41016b0bc6c814757
박건규 <geongyu09@gmail.com>
2026-04-25 00:49:36 +0900

    feat: CS 스터디 로그 데이터 추가 및 연도 계산 동적화
    HTTP 완벽 가이드, 운영체제 아주 쉬운 세 가지 이야기 스터디 데이터를 추가하고,
    현재 연도를 기준으로 GitHub 그래프 연도 목록을 동적으로 계산하도록 개선하였습니다.

--- src/app/log/_lib/getAllLogWithinYears.ts
+++ src/app/log/_lib/getAllLogWithinYears.ts
@@ -2,7 +2,12 @@ import STUDY_DATA from '@/constants/log/studyData';
 import { groupByYear, groupStudiesByYear } from '@/lib/log/groupByYear';
 import { getAllPosts } from '@/lib/post/post';
 
-const YEARS_WITH_GITHUB_GRAPH = [2024, 2025];
+const START_YEAR = 2024;
+const currentYear = new Date().getFullYear();
+const YEARS_WITH_GITHUB_GRAPH = Array.from(
+  { length: currentYear - START_YEAR + 1 },
+  (_, i) => START_YEAR + i,
+);
 
 const getAllLogWithinYears = () => {
   const allPosts = getAllPosts();
--- src/constants/log/CS_STUEY.ts
+++ src/constants/log/CS_STUEY.ts
@@ -69,12 +69,120 @@ const CS_STUDY: LogListItem[] = [
   },
   {
     title: '선이 없는 세상, 무선으로의 여행',
-    date: '2025-05-10',
+    date: '2025-01-10',
     href: 'https://geongyu09.notion.site/11-1b0edd40d49480ce9af3e722846e48b6?source=copy_link',
     tags: ['CS 스터디', '후니의 쉽게 쓴 CISCO 네트워킹'],
   },
 
   //
+  {
+    title: '[HTTP]1장 HTTP 개관',
+    date: '2025-03-17',
+    href: 'https://geongyu09.notion.site/HTTP-1-1b9edd40d494805db189d2f06ff40307?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: '인터넷의 리소스 탐색하기',
+    date: '2025-03-31',
+    href: 'https://geongyu09.notion.site/1-2-1-3-1c1edd40d4948099a066f7124138533c?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: 'TCP 커넥션',
+    date: '2025-04-06',
+    href: 'https://geongyu09.notion.site/1-4-1c8edd40d49480e7ab41c74277600033?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: '다체로운 웹 서버',
+    date: '2025-05-08',
+    href: 'https://geongyu09.notion.site/5-1ededd40d49480ce8ec2ec92b69f17c2?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: '캐시',
+    date: '2025-05-15',
+    href: 'https://geongyu09.notion.site/7-202edd40d494806791e1f4d62b5ec38d?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: '게이트웨이',
+    date: '2025-05-29',
+    href: 'https://geongyu09.notion.site/8-209edd40d4948025a858f732d02bba0c?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: '웹 로봇',
+    date: '2025-07-03',
+    href: 'https://geongyu09.notion.site/9-224edd40d494805a9337dcb7d924fb2e?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: 'HTTP2.0 / 11장 클라이언트 식별과 쿠키',
+    date: '2025-07-17',
+    href: 'https://geongyu09.notion.site/10-http2-0-11-22bedd40d494804b8485c2e3af24faf7?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: 'HTTP 인증',
+    date: '2025-08-07',
+    href: 'https://geongyu09.notion.site/http-240edd40d494809cb3dccd3c8e60d8b7?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: 'HTTPS',
+    date: '2025-08-28',
+    href: 'https://geongyu09.notion.site/https-24eedd40d49480e2a2ecdd71242ef2ea?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: '엔터티와 인코딩',
+    date: '2025-09-04',
+    href: 'https://geongyu09.notion.site/24eedd40d49480839fffe8eafafeb9c4?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: '국제화',
+    date: '2025-09-25',
+    href: 'https://geongyu09.notion.site/272edd40d49480e0a23ee018960a869f?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: '배포시스템',
+    date: '2025-11-06',
+    href: 'https://geongyu09.notion.site/28eedd40d49480a7b1e7d74fba3c7f22?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: '로깅과 사용자 추적',
+    date: '2025-11-13',
+    href: 'https://geongyu09.notion.site/28eedd40d49480ebbe77e0a876efb937?source=copy_link',
+    tags: ['CS 스터디', 'HTTP 완벽 가이드'],
+  },
+  {
+    title: '운영체제 개요',
+    date: '2025-12-04',
+    href: 'https://econovation.notion.site/2bf30b4e356c8010a774e560a155c914?source=copy_link',
+    tags: ['CS 스터디', '운영체제 아주 쉬운 세 가지 이야기'],
+  },
+  {
+    title: '시스템콜과 스케줄링',
+    date: '2026-01-01',
+    href: 'https://econovation.notion.site/2db30b4e356c80628d5ceb613a341cac?source=copy_link',
+    tags: ['CS 스터디', '운영체제 아주 쉬운 세 가지 이야기'],
+  },
+  {
+    title: '비례배분, 멀티프러세서',
+    date: '2026-01-15',
+    href: 'https://econovation.notion.site/2e930b4e356c80f0bb7efc2e0616da9c?source=copy_link',
+    tags: ['CS 스터디', '운영체제 아주 쉬운 세 가지 이야기'],
+  },
+  {
+    title: '주소 공간의 개념, 메모리 관리',
+    date: '2026-01-22',
+    href: 'https://econovation.notion.site/2f030b4e356c80d3b528cd1394a4fe64?source=copy_link',
+    tags: ['CS 스터디', '운영체제 아주 쉬운 세 가지 이야기'],
+  },
 ];
 
 export default CS_STUDY;
