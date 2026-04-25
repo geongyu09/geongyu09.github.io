0d480ed4f0bda99805cb27929b314bfbf03b4ff6
박건규 <geongyu09@gmail.com>
2026-04-25 09:53:37 +0900

    feat: 타입스크립트 스터디 데이터 추가 및 태그 레이아웃 개선

---
+++ src/constants/log/study-list/TS_STUDY.ts
@@ -0,0 +1,42 @@
+import { LogListItem } from '@/types/log';
+
+const TS_STUDY: LogListItem[] = [
+  {
+    title: `타입스크립트 개요, 기본 타입, 배열과 튜플`,
+    date: '2025-11-17',
+    href: 'https://geongyu09.notion.site/1-etc-28bedd40d49480698518f9e421bc05e4?source=copy_link',
+    tags: ['타입스크립트 스터디', '한 입 크기로 잘라먹는 타입스크립트'],
+  },
+  {
+    title: `객체, 타입 별칭과 인덱스 시그니쳐, 열거형 타입, any와 unknown, Void와 never, 타입 계층도`,
+    date: '2025-11-24',
+    href: 'https://econovation.notion.site/2-2b530b4e356c805fb827e74901c27e52?source=copy_link',
+    tags: ['타입스크립트 스터디', '한 입 크기로 잘라먹는 타입스크립트'],
+  },
+  {
+    title: `객체의 타입 호환성, 대수 타입, 타입 추론, 타입 단언, 타입 좁히기, 함수 타입, 호출 시그니쳐`,
+    date: '2025-12-22',
+    href: 'https://econovation.notion.site/3-2d130b4e356c801ba5fddb2473d31e32?source=copy_link',
+    tags: ['타입스크립트 스터디', '한 입 크기로 잘라먹는 타입스크립트'],
+  },
+  {
+    title: `함수 타입의 호환성, 함수 오버로딩, 타입 가드, 인터페이스, 인터페이스 확정, 인터페이스 선언 합치기`,
+    date: '2025-12-25',
+    href: 'https://econovation.notion.site/4-2d430b4e356c808b9e1ac414414e97f6?source=copy_link',
+    tags: ['타입스크립트 스터디', '한 입 크기로 잘라먹는 타입스크립트'],
+  },
+  {
+    title: `제네릭, 인덱스트 엑세스 타입, keyof%typeof, 맵드 타입`,
+    date: '2026-01-03',
+    href: 'https://econovation.notion.site/5-2dd30b4e356c80d09f94d3496d084e09?source=copy_link',
+    tags: ['타입스크립트 스터디', '한 입 크기로 잘라먹는 타입스크립트'],
+  },
+  {
+    title: `조건부 타입, 분산적인 조건부 타입, infer, 유틸리티 타입`,
+    date: '2026-01-12',
+    href: 'https://econovation.notion.site/6-2e630b4e356c8026aa45c0cec81a8f5d?source=copy_link',
+    tags: ['타입스크립트 스터디', '한 입 크기로 잘라먹는 타입스크립트'],
+  },
+];
+
+export default TS_STUDY;
--- src/constants/log/studyData.ts
+++ src/constants/log/studyData.ts
@@ -3,12 +3,14 @@ import JAASSTUDY from './study-list/JAASSTUDY';
 import DONUT_STUDY from './study-list/DONUT_STUDY';
 import CS_STUDY from './study-list/CS_STUEY';
 import MODERN_JS_STUDY from './study-list/MODERN_JS_STUDY';
+import TS_STUDY from './study-list/TS_STUDY';
 
 const STUDY_DATA: LogListItem[] = [
   ...JAASSTUDY,
   ...DONUT_STUDY,
   ...CS_STUDY,
   ...MODERN_JS_STUDY,
+  ...TS_STUDY,
 ];
 
 export default STUDY_DATA;
