f2d4453f83f118a99be3c675cab36d0b070c7dc6
박건규 <geongyu09@gmail.com>
2026-04-25 09:42:09 +0900

    feat: 스터디 데이터 구조 리팩토링 및 모던 JS 스터디 추가
    스터디 데이터 파일을 study-list/ 하위 디렉토리로 이동하고,
    모던 자바스크립트 스터디 데이터(MODERN_JS_STUDY)를 추가하였습니다.
    연도별 요약 bullet 생성 유틸 함수(getYearSummaryBullets)도 추가하였습니다.

---
+++ src/constants/log/study-list/MODERN_JS_STUDY.ts
@@ -0,0 +1,113 @@
+import { LogListItem } from '@/types/log';
+
+const MODERN_JS_STUDY: LogListItem[] = [
+  {
+    title: '논리 연산자, 반복문, 함수, 함수 표현식',
+    date: '2025-07-08',
+    href: 'https://geongyu09.notion.site/2-22aedd40d49480f4b759dd0714d5fbeb?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: `가비지 컬랙션, 메서드와 this, new 연산자와 생성자 함수, 옵셔널 체이닝 '?.', 심볼형, 객체를 원시형으로 변환하기`,
+    date: '2025-07-15',
+    href: 'https://geongyu09.notion.site/3-231edd40d494807c9f5cc6dc2a5fab4f?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: '자료구조와 자료형, 맵과 셋',
+    date: '2025-07-29',
+    href: 'https://geongyu09.notion.site/23fedd40d494800ebcb6df346e541c77?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: `위크맵과 위크셋, Object.keys/values/entries, 구조 분해 할당, Date 객체와 날짜, JSON과 메서드, 재귀와 스택, 나머지 매개변수와 전개 구문, 변수의 유효범위와 클로저`,
+    date: '2025-08-05',
+    href: 'https://geongyu09.notion.site/5-246edd40d49480d1bcc1f668b1993869?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title:
+      '오래된 var, 전역 객체, 객체로서의 함수와 기명 함수 표현식, new Function, setTimeout과 setInterval을 이용한 호출 스케줄링, call/apply와 데코레이터/포워딩, 함수 바인딩, 화살표 함수 다시 살펴보기',
+    date: '2025-08-12',
+    href: 'https://geongyu09.notion.site/6-_-24dedd40d49480a5a931db40fa66dcf3?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: `중간 정리(형 변환, 객체, 실행 컨텍스트, 클로저와 렉시컬 환경, this바인딩)`,
+    date: '2025-08-19',
+    href: 'https://geongyu09.notion.site/254edd40d494807cb0aad0eb47f8f385?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: `객체 프로퍼티와 프로토타입 상속`,
+    date: '2025-08-26',
+    href: 'https://geongyu09.notion.site/8-25bedd40d49480bdb49cd2315ac4d0cb?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: '클래스',
+    date: '2025-09-05',
+    href: 'https://geongyu09.notion.site/265edd40d4948077aa15d3d40da5a05b?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: '에러 핸들링',
+    date: '2025-09-08',
+    href: 'https://geongyu09.notion.site/268edd40d4948075bcf0d13bdb8a753f?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title:
+      '콜백, 프라미스, 프라미스 체이닝, 프라미스와 에러 핸들링, 프라미스 API',
+    date: '2025-09-15',
+    href: 'https://geongyu09.notion.site/26fedd40d494802e95fef10b333dfe4c?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: '비동기와 제너레이터',
+    date: '2025-09-22',
+    href: 'https://geongyu09.notion.site/276edd40d49480078848e4971e7791f7?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: '모듈',
+    date: '2025-09-30',
+    href: 'https://geongyu09.notion.site/27eedd40d49480d084dfe2421e8c0b46?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: 'Eval, 커링, 참조 타입, BigInt',
+    date: '2025-10-13',
+    href: 'https://geongyu09.notion.site/1-etc-28bedd40d49480698518f9e421bc05e4?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+
+  // DOM
+
+  {
+    title: '기히 프로퍼티',
+    date: '2025-11-21',
+    href: 'https://econovation.notion.site/2b230b4e356c8000a03cf79421255dd9?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: `버블링과 캡처링 & 이벤트 위임 & 브라우저 기본 동작 & 커스텀 이벤트 디스패치`,
+    date: '2026-01-01',
+    href: 'https://econovation.notion.site/4-2dd30b4e356c805b9d17ca55260d24e0?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: `마우스 이벤트, 드래그 앤 드롭, 폼 프로퍼티와 메서드, focus와 blur`,
+    date: '2026-01-03',
+    href: 'https://econovation.notion.site/2db30b4e356c8083b8c0cdbad29c0b7e?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+  {
+    title: `이벤트/DOMContentLoaded, defer-async, 이벤트루프`,
+    date: '2026-01-11',
+    href: 'https://econovation.notion.site/DOMContentLoaded-defer-async-2e530b4e356c80439bb7e91583686e8d?source=copy_link',
+    tags: ['모던 자바스크립트 스터디', '모던 JavaScript 튜토리얼'],
+  },
+];
+
+export default MODERN_JS_STUDY;
--- src/types/log.ts
+++ src/types/log.ts
@@ -1,10 +1,15 @@
 type LogTag =
   | '자아스스터디'
   | 'CS 스터디'
+  //
   | '후니의 쉽게 쓴 CISCO 네트워킹'
   | '코어자바스크립트'
+  //
   | 'HTTP 완벽 가이드'
-  | '운영체제 아주 쉬운 세 가지 이야기';
+  | '운영체제 아주 쉬운 세 가지 이야기'
+  //
+  | '모던 자바스크립트 스터디'
+  | '모던 JavaScript 튜토리얼';
 
 export interface LogListItem {
   title: string;
