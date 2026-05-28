e6e53988b203de14563e63dddfe5e23bfd22b2ad
박건규 <geongyu09@gmail.com>
2026-05-28 12:47:38 +0900

    refactor: ShortTableOfContent 인라인 스타일화 및 사용 안 하는 컴포넌트 제거
    레벨별 H1/H2/H3Content 컴포넌트 분기를 제거하고 단일 div에
    레벨별 너비 매핑으로 단순화하였으며, 활성 헤딩 강조를 추가하였습니다.
    더 이상 참조되지 않는 sideContentTable 디렉토리를 제거하였습니다.

--- src/components/common/sideContentTable/components/H1Content/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function H1Content() {
-  return <div className="w-10 rounded-full bg-slate-200 h-1" />;
-}
--- src/components/common/sideContentTable/components/H3Content/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function H3Content() {
-  return <div className="w-6 rounded-full bg-slate-200 h-1" />;
-}
